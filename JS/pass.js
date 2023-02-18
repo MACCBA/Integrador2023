3918f4c177fced2a6be27bbab40c3464e6ceaf5f
MACCBA <mac88_13@hotmail.com>
2023-02-18 02:58:56 -0300

    Update pass.js

--- JS/pass.js
+++ JS/pass.js
@@ -5,7 +5,7 @@ function login () {
     password = document.getElementById("passwordL").value;
    
     if ( user =="Admin" && password == "123456"){
-        window.open  ("http://127.0.0.1:5500/index.html");
+        window.open  ("https://integrador2023.vercel.app/");
     } else {
         alert ("Datos inccorrectos, favor de inentar nuevamente")
     }
